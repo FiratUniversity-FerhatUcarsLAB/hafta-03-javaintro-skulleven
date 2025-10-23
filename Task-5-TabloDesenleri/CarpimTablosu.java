9f1b34d5b5e08e7259c615a9e37f07e3baf5b08e
Skulleven <skulydon@outlook.com>
2025-10-23 13:30:58 +0300

    Final Commit (hopefully)

--- Task-5-TabloDesenleri/CarpimTablosu.java
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -1,12 +1,7 @@
 class CarpimTablosu{
     public static void main(String[] args) {
-        
-        System.out.println("a       a^2     a^3");
-        System.out.println("======================");
-        System.out.println("1       " + Math.pow(1, 2) + "     " + Math.pow(1, 3));
-        System.out.println("2       " + Math.pow(2, 2) + "     " + Math.pow(2, 3));
-        System.out.println("3       " + Math.pow(3, 2) + "     " + Math.pow(3, 3));
-        System.out.println("4       " + Math.pow(4, 2) + "    " + Math.pow(4, 3));
-        System.out.println("5       " + Math.pow(5, 2) + "    " + Math.pow(5, 3));
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("2   x   " + i + "   =   " + (i*2));
+        }
     }
 }
--- Task-5-TabloDesenleri/UsluSayilarTablosu.java
+++
@@ -1,7 +0,0 @@
-class UsluSayilarTablosu{
-    public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.println("2   x   " + i + "   =   " + (i*2));
-        }
-    }
-}
